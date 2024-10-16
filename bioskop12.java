32fd1a8d23565120bdfe1a566cb1eac7863c3f0e
damielassaqovi <palurejodaniel@gail.com>
2024-10-16 20:32:15 +0700

    tugas

--- bioskop12.java
+++ bioskop12.java
@@ -1,36 +1,73 @@
-
 import java.util.Scanner;
 
-public class bioskop12{
-  public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
+public class bioskop12 {
+    
+    public static void main(String[] args) {
+        
+        //deklarasi inputan
+        Scanner input = new Scanner(System.in);
 
-      String namaPelanggan;
-      int Htiket = 50000, jmlTiket = 0;
-      Double diskon, total;
+        //deklarasi variable
+        int hargaTiket = 50000;
+        int jml_tiket, total_tiket_terjual = 0;
+        float diskon = 0, total_harga = 0, harga_diskon = 0, total_penjualan = 0;
+        boolean isLanjut = true;
 
-      do { 
-          System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar):");
-          namaPelanggan = sc.nextLine();
-          if (namaPelanggan.equalsIgnoreCase("batal")){
-            System.out.println("Transaksi dibatalkan");
-            break;
-          }
-          System.out.print("Masukkan jumlah tiket\t: ");
-          jmlTiket = sc.nextInt();
-          if (jmlTiket > 4 ) {
-              diskon = 0.1;   
-              }else if (jmlTiket > 10) {
-                  diskon = 0.15;
-          } else {
-              diskon = 0.0;
-          }
-          total = jmlTiket * Htiket - (jmlTiket * Htiket * diskon);
-          System.out.println("Total harga yang harus dibayar\t: " + total);
-          sc.nextLine();
-      } while (true);
+        //judul
+        System.out.println("=========================================");
+        System.out.println("======= Perhitungan Tiket Bioskop =======");
+        System.out.println("=========================================");
 
-      System.out.println("Semua transaksi selesai.");
+        //logic dan inputan
+        while (isLanjut) {
 
-  }
+            //input jumlah tiket yang akan dibeli
+            System.out.print("Masukkan jumlah tiket : ");
+            jml_tiket = input.nextInt();
+
+            //jika input negatif
+            if (jml_tiket < 0) {
+                System.out.println("Jumlah tidak bisa negatif, masukkan jumlah yg benar!");
+                continue;
+            }
+
+            //jika input valid
+            //menghitung jumlah tiket yg terjual
+            total_tiket_terjual += jml_tiket;
+
+            //hitung total harga (bayar)
+            total_harga = jml_tiket * hargaTiket;
+
+            //hitung harga (bayar) setelah diskon
+            if (jml_tiket > 10) {
+                diskon = 0.15f;
+                harga_diskon = total_harga - (total_harga * diskon);
+            }
+            else if (jml_tiket > 4) {
+                diskon = 0.1f;
+                harga_diskon = total_harga - (total_harga * diskon);
+            }
+            else {
+                harga_diskon = total_harga;
+            }
+
+            //total penjualan
+            total_penjualan += harga_diskon;
+
+            //output untuk transaksi sementara
+            System.out.println("Total harga untuk " + jml_tiket + " tiket adalah Rp " + harga_diskon);
+
+            //input isLanjut
+            System.out.print("Apakah lanjut? (true / false) : ");
+            isLanjut = input.nextBoolean();
+
+        };
+
+        //output data semua transaksi
+        System.out.println("=========================================");
+        System.out.println("Total tiket terjual\t : " + total_tiket_terjual);
+        System.out.println("Total penjualan\t\t : " + total_penjualan);
+
+    }
+
 }
